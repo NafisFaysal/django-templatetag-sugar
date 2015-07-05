8c7565ae6dcde0020976652d8f32b823de303899
Nafis Faysal <NafisFaysal@users.noreply.github.com>
2015-07-05 15:54:02 +0600

    update model

---
+++ templatetag_sugar/models.py
@@ -0,0 +1,51 @@
+from django.db import models
+from django.core.urlresolvers import reverse
+
+from ckeditor.fields import RichTextField
+from tagging.fields import TagField
+
+
+class Categories(models.Model):
+    name = models.CharField(max_length=50, unique=True)
+    common_name = models.common_nameField(max_length=50, unique=True)
+
+    class Meta:
+        ordering = ['name']
+        verbose_name_plural = "Categories"
+
+    def get_absolute_url(self):
+        return reverse ('category_view', kwargs={'common_name': self.common_name})
+
+
+    def __unicode__(self):
+        return self.name
+
+
+
+
+class Blogs(models.Model):
+    page_title = models.CharField(max_length=150, unique=True, verbose_name='Page Title', blank=True)
+    title = models.CharField(max_length=150, unique=True, verbose_name='Title')
+    common_name = models.common_nameField(max_length=150, unique=True, verbose_name='URL')
+    date = models.DateField(auto_now=True)
+    time = models.TimeField(auto_now=True)
+    meta_description = models.CharField(max_length=500, verbose_name='Meta Description', blank=True)
+    meta_keywords = models.CharField(max_length=250, verbose_name='Meta Keywords', blank=
+True)
+    body = RichTextField()
+    category = models.ForeignKey(Categories)
+    publish = models.BooleanField(default=None)
+    tags = TagField()
+
+    class Meta:
+        ordering = ['-date']
+        verbose_name_plural = "Blogs"
+
+
+    def get_absolute_url(self):
+        return reverse ('blog_view', kwargs={'common_name': self.common_name})
+
+
+    def __unicode__(self):
+        return self.title 
+
